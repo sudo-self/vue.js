150d446e094a95447be9c315f1d35ffc36039538
Sudo-Self <sudo-self@proton.me>
2024-01-20 02:51:41 -0700

    Add files via upload

---
+++ views/AboutView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about">
+    <h1>AboutView.vue</h1>
+  </div>
+<p><code>localhost:5173/about</code></p>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
---
+++ views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+  <p><code>Written in TypeScript, VUE.js is an open-source frontend Java library for building UI and single-page applications.</code></p>
+</template>
